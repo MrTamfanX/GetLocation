356481742e686a360633e4c10011b32e7b3b1eff
MrTamfanX <prakasaputra349@gmail.com>
2021-03-06 23:20:20 +0700

    GPS By UYSFDS-INDO

---
+++ app/src/main/java/com/uysfdsindo/gps/MainActivity.java
@@ -0,0 +1,289 @@
+package com.uysfdsindo.gps;
+
+import android.app.Activity;
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+import android.content.*;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.webkit.*;
+import android.animation.*;
+import android.view.animation.*;
+import java.util.*;
+import java.util.regex.*;
+import java.text.*;
+import org.json.*;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.ImageView;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationListener;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.DialogFragment;
+import android.Manifest;
+import android.content.pm.PackageManager;
+
+
+public class MainActivity extends  Activity { 
+	
+	
+	private String strAdd = "";
+	private String strCity = "";
+	private String strState = "";
+	private String strCountry = "";
+	private String strPC = "";
+	private String strKN = "";
+	
+	private LinearLayout linear1;
+	private LinearLayout linear2;
+	private TextView textview1;
+	private TextView textview2;
+	private ImageView imageview1;
+	private TextView textview3;
+	private TextView textview4;
+	private TextView textview5;
+	private TextView textview6;
+	private TextView textview7;
+	private TextView textview8;
+	
+	private LocationManager lm;
+	private LocationListener _lm_location_listener;
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.main);
+		initialize(_savedInstanceState);
+		if (Build.VERSION.SDK_INT >= 23) {
+			if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED) {
+				requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1000);
+			}
+			else {
+				initializeLogic();
+			}
+		}
+		else {
+			initializeLogic();
+		}
+	}
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+		if (requestCode == 1000) {
+			initializeLogic();
+		}
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		
+		linear1 = (LinearLayout) findViewById(R.id.linear1);
+		linear2 = (LinearLayout) findViewById(R.id.linear2);
+		textview1 = (TextView) findViewById(R.id.textview1);
+		textview2 = (TextView) findViewById(R.id.textview2);
+		imageview1 = (ImageView) findViewById(R.id.imageview1);
+		textview3 = (TextView) findViewById(R.id.textview3);
+		textview4 = (TextView) findViewById(R.id.textview4);
+		textview5 = (TextView) findViewById(R.id.textview5);
+		textview6 = (TextView) findViewById(R.id.textview6);
+		textview7 = (TextView) findViewById(R.id.textview7);
+		textview8 = (TextView) findViewById(R.id.textview8);
+		lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		
+		_lm_location_listener = new LocationListener() {
+			@Override
+			public void onLocationChanged(Location _param1) {
+				final double _lat = _param1.getLatitude();
+				final double _lng = _param1.getLongitude();
+				final double _acc = _param1.getAccuracy();
+				_getLocation(_lat, _lng);
+				textview3.setText("Jalan: ".concat(strAdd));
+				textview4.setText("Kecamatan: ".concat(strCity));
+				textview5.setText("Kota/Kabupaten: ".concat(strState));
+				textview6.setText("Negara: ".concat(strCountry));
+				textview7.setText("Kode pos: ".concat(strPC));
+				textview8.setText("Nama jalan: ".concat(strKN));
+			}
+			@Override
+			public void onStatusChanged(String provider, int status, Bundle extras) {}
+			@Override
+			public void onProviderEnabled(String provider) {}
+			@Override
+			public void onProviderDisabled(String provider) {}
+		};
+	}
+	
+	private void initializeLogic() {
+		if (Build.VERSION.SDK_INT >= 23) {if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+				lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, _lm_location_listener);
+			}
+		}
+		else {
+			lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, _lm_location_listener);
+		}
+		_NavStatusBarColor("#FFFFFF", "#FFFFFF");
+		_DARK_ICONS();
+		_SetCornerRadius(linear2, 16, 20, "#FFFFFF");
+	}
+	
+	@Override
+	protected void onActivityResult(int _requestCode, int _resultCode, Intent _data) {
+		
+		super.onActivityResult(_requestCode, _resultCode, _data);
+		
+		switch (_requestCode) {
+			
+			default:
+			break;
+		}
+	}
+	
+	@Override
+	public void onStart() {
+		super.onStart();
+		lm.removeUpdates(_lm_location_listener);
+		SketchwareUtil.showMessage(getApplicationContext(), "Tunggu Sebentar Untuk Lokasi Anda...");
+		if (Build.VERSION.SDK_INT >= 23) {if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+				lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, _lm_location_listener);
+			}
+		}
+		else {
+			lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, _lm_location_listener);
+		}
+	}
+	public void _getLocation (final double _LATITUDE, final double _LONGITUDE) {
+		android.location.Geocoder geocoder = new android.location.Geocoder(getApplicationContext(), Locale.getDefault());
+		
+		try {
+			List<android.location.Address> addresses = geocoder.getFromLocation(_LATITUDE, _LONGITUDE, 1);
+			if (addresses != null) {
+				android.location.Address returnedAddress = addresses.get(0);
+				StringBuilder strReturnedAddress = new StringBuilder("");
+				StringBuilder strReturnedCity = new StringBuilder("");
+				StringBuilder strReturnedState = new StringBuilder("");
+				StringBuilder strReturnedCountry = new StringBuilder("");
+				StringBuilder strReturnedPC = new StringBuilder("");
+				StringBuilder strReturnedKN = new StringBuilder("");
+				for (int i = 0; i <= returnedAddress.getMaxAddressLineIndex(); i++) {
+					strReturnedAddress.append(returnedAddress.getAddressLine(i));
+					strReturnedCity.append(returnedAddress.getLocality()); 
+					strReturnedState.append(returnedAddress.getAdminArea());
+					strReturnedCountry.append(returnedAddress.getCountryName());
+					strReturnedPC.append(returnedAddress.getPostalCode());
+					strReturnedKN.append(returnedAddress.getFeatureName());
+				}
+				strAdd = strReturnedAddress.toString();
+				strCity = strReturnedCity.toString();
+				strState = strReturnedState.toString();
+				strCountry = strReturnedCountry.toString();
+				strPC = strReturnedPC.toString();
+				strKN = strReturnedKN.toString();
+			}
+			else
+			{
+				strAdd = "No Address returned";
+				strCity = "No City returned";
+				strState = "No State returned";
+				strCountry = "No Country returned";
+				strPC = "No Postal Code returned";
+				strKN = "No Know Name returned";
+				
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			strAdd = "Tidak dapat mengetahui jalan";
+			strCity = "Tidak dapat mengetahui kecamatan";
+			strState = "Tidak dapat mengetahui kota";
+			strCountry = "Tidak dapat mengetahui negara";
+			strPC = "Tidak dapat mengetahui kode pos";
+			strKN = "Tidak dapat mengetahui nama jalan";
+		}
+	}
+	
+	
+	public void _NavStatusBarColor (final String _color1, final String _color2) {
+		if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+			Window w = this.getWindow();	w.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);	w.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+			w.setStatusBarColor(Color.parseColor("#" + _color1.replace("#", "")));	w.setNavigationBarColor(Color.parseColor("#" + _color2.replace("#", "")));
+		}
+	}
+	
+	
+	public void _DARK_ICONS () {
+		getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+	}
+	
+	
+	public void _SetCornerRadius (final View _view, final double _radius, final double _shadow, final String _color) {
+		android.graphics.drawable.GradientDrawable ab = new android.graphics.drawable.GradientDrawable();
+		
+		ab.setColor(Color.parseColor(_color));
+		ab.setCornerRadius((float) _radius);
+		_view.setElevation((float) _shadow);
+		_view.setBackground(ab);
+		
+		//Add More block in OnCreateActivity :
+	}
+	
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input){
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels(){
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels(){
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+	
+}
